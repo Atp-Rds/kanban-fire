bdcc07b2b55776c07afd84112d845acdcbca2ea3
Atp-Rds <13067124+Atp-Rds@users.noreply.github.com>
2021-01-26 13:57:36 +0100

    First commit: basic drag&drop

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.2.0.
 
+## Description
+
+SPA to test Angular & FireBase
+Codelab: [Building a web application with Angular and Firebase](https://developers.google.com/codelabs/building-a-web-app-with-angular-and-firebase#1)
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+import { eventNames } from 'process';
 
+import { Task } from './task/task';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kanban-fire';
+    title = 'kanban-fire';
+
+    todo : Task[] = [
+        {
+            title: 'Buy milk',
+            description: 'Go to the store and buy milk'
+          },
+          {
+            title: 'Create a Kanban app',
+            description: 'Using Firebase and Angular create a Kanban app!'
+          }
+    ];
+
+    inProgress: Task[] = [];
+    done: Task[] = [];
+
+    editTask(list: string, task: Task): void {}
+
+    drop(event: CdkDragDrop<Task[]>): void {
+        if (event.previousContainer === event.container) {
+            return;
+        }
+        transferArrayItem(
+            event.previousContainer.data,
+            event.container.data,
+            event.previousIndex,
+            event.currentIndex
+        )
+    }
 }
